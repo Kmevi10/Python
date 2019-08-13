dbf810c41d5a6cad3ac10464479b0993cd9d5423
Carlos <carlos.mesa@adaits.com>
2019-08-13 10:32:56 +0200

    Inicio

---
+++ prueba.py
@@ -0,0 +1,14 @@
+print("introduce un número del 1 al 10")
+num = input()
+print("¿Ese es tu número favorito?")
+print("1. Si")
+print("2. No")
+opc = input()
+opcion = opc.upper()
+if opcion == "SI" or opc == 1:
+
+	print("¡Qué bien!")
+
+else:
+
+	print("¡OH! Que pena.")
